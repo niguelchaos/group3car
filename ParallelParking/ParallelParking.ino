de8a057b80f9e69a69ea90cdc085cfdfc6729032
fabianfroding <fabian.froding@gmail.com>
2018-04-17 20:34:52 +0200

    Removed redundant data
    Removed unused functions and variables from my branch.

--- ParallelParking/ParallelParking.ino
+++ ParallelParking/ParallelParking.ino
@@ -15,8 +15,6 @@ const int ECHO_PIN_RIGHT = 13;
 GP2Y0A21 rearIR; // are we using infrared here??
 const int rearIR_PIN = 00; //don't know, need to check car
 
-const int CAR_SIZE = 20;
-
 const double CAR_LENGTH = 25; // Car length
 const double CAR_WIDTH = 15.5; // Car Width
 
@@ -77,75 +75,6 @@ void handleInput() { //handle serial input if there is any
         car.setAngle(0);
     }
   }
-}
-
-void detectSpotSize() {
-  odometerRight.begin();
-  //float parkingSize = 0;
-  //parkMode = true;
-
-  // if we have begun detecting a spot and then we once again detect an obstacle, and the spot is greater than
-  // the car size, commence parking.
-  // P.S I don't think we need the first condition in the OR statement below, but I'm still considering.
-  //if(right.getDistance() <= 10 || encoderRight.distance() > CAR_SIZE * 2) {
-  if(rightSound.getDistance() <= 10 && odometerRight.getDistance() > CAR_SIZE * 2 || rightSound.getDistance() == 0 && odometerRight.getDistance() > CAR_SIZE * 2) {
-      park();
-  }
-}
-
-// Automatic parking
-void park() {
-  Serial.println(odometerRight.getDistance());
-}
-
-//========== Get the Size of a Parking Spot ==========//
-double getParkingSpotSize(Odometer odometer) {
-  boolean obstacleDetected = false;
-
-  parkMode = true;
-  car.setSpeed(20);
-  odometer.begin();
-  car.begin();
-
-  double initialSideDistance = rightSound.getDistance();
-
-  /*
-    If the initial side distance is too tight then start looking for a spot
-    that has a width that is greater than the car width.
-  */
-  if (initialSideDistance <= CAR_WIDTH) {
-    while(rightSound.getDistance() <= initialSideDistance) {
-      // Do nothing
-    }
-  }
-
-  /*
-    Once the distance to the right is large enough
-    we can start looking for the next obstacle that has a distance less then or equal to the car width
-    Or, until the distance traveled is greater than the car length
-  */
-
-  while(!obstacleDetected) {
-    double d = rightSound.getDistance();
-
-    // Stop if an obstacle is detected
-    if (d <= CAR_WIDTH) {
-      obstacleDetected = true;
-    }
-
-    // Stop if distance traveled is greater than or equal to twice the car size
-    if (odometer.getDistance() >= CAR_LENGTH * 2) {
-      obstacleDetected = true;
-    }
-
-  }
-
-  //----- Cleanup -----//
-  car.stop();
-  parkMode = false;
-
-  Serial.println("return value:" + odometer.getDistance());
-  return odometer.getDistance();
 }
 
 //========== Check if parking spot size is large enough ==========//
